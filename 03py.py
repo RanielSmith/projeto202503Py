41f4e92a2646f3345eabf468df0956d2f38761ed
RanielSmith <143230725+RanielSmith@users.noreply.github.com>
2023-08-25 21:36:58 -0300

    Add files via upload

---
+++ 03py.py
@@ -0,0 +1,23 @@
+print(8*2)
+print(12*12 ,50)
+print(2025-1992)
+print(2050-2023)
+print("Em que ano você nasceu")
+nascimento=input()
+nascimento=int(nascimento)
+print( 2025 - nascimento )
+print( "Em 2025 você terá", 2025- nascimento, "anos!")
+print(" VOCÊ TERÁ QUE IDADE?")
+x=input()
+x=int(x)
+x-nascimento
+print("quantos anos você tem?")
+
+print(" VOCÊ SERÁ SUA IDADE DE CACHORRO?")
+x=input()
+x=int(x)
+print("SE você fosse um cachorro", "você teria", x*7, "anos!")
+
+  '0'_____'
+     || ||
+
